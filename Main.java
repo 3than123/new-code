083e268e967a236bf0322f06c5f898b80ace9bab
3than123 <ethanlangle@code22-325.rockhurst.int>
2023-03-29 11:42:02 -0500

    last change

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
     System.out.println("Hello World");
     System.out.println("Hello");
     System.out.println("world");
-    System.out.println("third time doign it ");
+    System.out.println("third time doing it ");
     System.out.println("last time");
 
 }
